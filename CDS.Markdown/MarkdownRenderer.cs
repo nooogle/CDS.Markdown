1b6f4cfe56b5d51427cf2fa9ee0748cd822533c3
Jon Shadforth <jon@carpediemsystems.co.uk>
2025-08-09 09:05:14 +0100

    Enhance MarkdownRenderer with emoji support
    Updated the `MarkdownRenderer` constructor to include the `UseEmojiAndSmiley()` method, improving the Markdown rendering capabilities by enabling the use of emojis and smileys.

--- CDS.Markdown/MarkdownRenderer.cs
+++ CDS.Markdown/MarkdownRenderer.cs
@@ -19,6 +19,7 @@ public class MarkdownRenderer
     {
         pipeline = new MarkdownPipelineBuilder()
             .UseAdvancedExtensions()
+            .UseEmojiAndSmiley()
             .Build();
     }
 
